4183de919f192c7dd268b7c1f1f163efe783d5cd
KunalNasa <kunalnasa.dev@gmail.com>
2025-11-01 14:50:48 +0530

    update neovim config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -40,6 +40,8 @@ return {
     -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
     -- and elegantly composed help section, `:help lsp-vs-treesitter`
 
+    -- disable LSP logging to avoid performance and freeze issues
+    vim.lsp.set_log_level 'OFF'
     --  This function gets run when an LSP attaches to a particular buffer.
     --    That is to say, every time a new file is opened that is associated with
     --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
